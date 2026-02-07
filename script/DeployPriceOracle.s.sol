fa7d643130a0bc6ece4ca0df02fbf15e6113be86
fraolb <Fraolbereket@gmail.com>
2026-02-07 15:28:17 +0300

    minUpdate interval change

--- script/DeployPriceOracle.s.sol
+++ script/DeployPriceOracle.s.sol
@@ -6,18 +6,13 @@ import {PriceOracle} from "../src/PriceOracle.sol";
 
 contract DeployPriceOracle is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envOr("PRIVATE_KEY", uint256(0));
-        
-        // If no private key is provided (local test), use a dummy address or default sender
-        // For actual deployment, startBroadcast(deployerPrivateKey) would be used.
-        // For this script, we'll just use startBroadcast() which uses the default foundry sender.
         
         vm.startBroadcast();
 
         // Configuration
         address admin = msg.sender;
         uint256 maxDeviation = 500; // 5%
-        uint256 minUpdateInterval = 3600; // 1 hour
+        uint256 minUpdateInterval = 300; // 5 min
 
         new PriceOracle(admin, maxDeviation, minUpdateInterval);
 
